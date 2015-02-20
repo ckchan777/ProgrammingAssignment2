6ad27b177d8dc9b9eac494fc5282dc026c8c65d4
ckchan777 <zeng.jiaguang@gmail.com>
2015-02-20 21:44:51 +0800

    lack matrix = new matrix check

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,24 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-##tester
+
+    ##assume x is a square matrix, no exception handlers for this part  
+    m<-NULL
+    set<-function(y)
+    {
+      x<<-y
+      m<<-NULL  ##uses <<- operator
+    }
+    get<-function() x
+    
+    ##solve returns the inverse of the matrix argument
+    setinversematrix<-function(m) 
+    {
+      solve(m)
+    }
+    
+    ##returns the matrix
+    getinversematrix<- function() m  
 }
 
 
@@ -12,4 +29,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getinversematrix()
+    
+    ##if m is NOT null, implies cached data exists and thus returns that data
+    if(!is.null(m)) ##AND must also check the SAME matrix is store in m
+    {
+      message("getting cached data")
+      return(m)
+    }
+    
+    ##if m is null, this block will be executed, no cache data available
+    ##thus must calculate a new inverse matrix 
+    data<-x$get() ##data stores the user input matrix 
+    
+    ##assigns variable m with the inverse of the matrix
+    ##after this is executed, if(!is.null(m)) will be TRUE and there will be a cached copy of the matrix
+    m<-store(data)
+    x$setinversematrix(m)
+    m
+    
 }
